c15bd04bb617b1c687146dab0bba828270357fdc
Tiago Possato <tiago.possato@ifc-videira.edu.br>
2016-08-30 12:32:56 +0000

    versao 0.0.1

--- assets/maquina-turing.js
+++ assets/maquina-turing.js
@@ -1,66 +1,102 @@
-function MaquinaDeEstados() {
+/*global $*/
+
+function MaquinaDeTuring() {
     var alfabetoInicial;
     var alfabetoFinal;
+    var alfabetoNaFita;
     var simboloVazio;
-    var qtdEstados;
+    // var qtdEstados;
     var estados;
     var estadoInicial;
 
-    this.qtdEstados = 0;
+    // this.qtdEstados = 0;
     this.estados = [];
     this.alfabetoFinal = [];
     this.alfabetoInicial = [];
+    this.alfabetoNaFita = "";
     this.estadoInicial = false;
 
 };
 
-function Estado(_id) {
-    var id;
+function Estado(_q) {
+    var q;
     var transicoes;
     var estadoFinal;
+    var estadoInicial;
 
-    this.id = _id;
+    this.q = _q;
     this.transicoes = [];
     this.estadoFinal = false;
+    this.estadoInicial = false
 
 };
 
-function Transicao(_id) {
-    var id;
+function Transicao() {
     var proximoEstado;
     var lidoNaFita;
     var escritoNaFita;
     var movimento;
-
-    this.id = _id;
+    
     this.proximoEstado = 0;
     this.lidoNaFita = null;
     this.escritoNaFita = null;
     this.movimento = 0;
 };
 
-var maquinaDeEstados;
+var maquinaDeTuring;
 
-$(window).load(function() {
-    maquinaDeEstados = new MaquinaDeEstados();
+var calculaMaquina = function(mq) {
+    console.log(mq);
+    var cabecote = 0;
+    var estadoAtual = mq.estadoInicial;
 
-    var dados = localStorage.getItem("maquinaDeEstados"); // Recupera os dados armazenados
-    if (dados === null) return;
+    console.log("Movimentos = {");
 
-    maquinaDeEstados = JSON.parse(dados);
-    maquinaDeEstados.estados = [];
+    while (cabecote < mq.alfabetoNaFita.length) {
+        var palavraValida = false;
 
-    for (var i = 0; i < maquinaDeEstados.alfabetoInicial.length; i++) {
-        $('#alfabetoInicial').tagsinput('add', maquinaDeEstados.alfabetoInicial[i]);
+        //console.log("Fita[" + cabecote + "] = " + mq.alfabetoNaFita[cabecote]);
+        // console.log("estadoAtual:" + estadoAtual);
+        var saida = "\t(Q[" + estadoAtual + "], Lido: " + mq.alfabetoNaFita[cabecote] + ") => (";
+        for (var i = 0; i < mq.estados[estadoAtual].transicoes.length; i++) {
+            if (mq.estados[estadoAtual].transicoes[i].lidoNaFita === mq.alfabetoNaFita[cabecote]) {
+
+                saida += "(Q[" + mq.estados[estadoAtual].transicoes[i].proximoEstado + "], Escrito: " + mq.estados[estadoAtual].transicoes[i].escritoNaFita +
+                    " , Mov: " + mq.estados[estadoAtual].transicoes[i].movimento + ")";
+
+                mq.alfabetoNaFita = setCharAt(mq.alfabetoNaFita, cabecote, mq.estados[estadoAtual].transicoes[i].escritoNaFita);
+
+                //mq.alfabetoFinal = mq.alfabetoFinal.replaceAt(cabecote, mq.estados[estadoAtual].transicoes[i].escritoNaFita);
+                cabecote += parseInt(mq.estados[estadoAtual].transicoes[i].movimento); //movimenta o cabeçote de leitura
+                estadoAtual = parseInt(mq.estados[estadoAtual].transicoes[i].proximoEstado);
+
+                palavraValida = true;
+
+                // console.log("Transicao encontrada no estado: ");
+                // console.log(mq.estados[estadoAtual].transicoes[i]);
+
+                break;
+            }
+        }
+        // console.log("proximoEstado: " + estadoAtual);
+
+        if (!palavraValida) {
+            console.log("Nenhuma Transicao encontrada na posicao: " + cabecote + ", Caracter: " + mq.alfabetoNaFita[cabecote]);
+            //console.log("A palavra não é válida!");
+            break;
+        }
+
+        console.log(saida);
+
     }
-
-    for (var i = 0; i < maquinaDeEstados.alfabetoFinal.length; i++) {
-        if (maquinaDeEstados.alfabetoFinal[i] === maquinaDeEstados.simboloVazio) continue;
-        $('#alfabetoFinal').tagsinput('add', maquinaDeEstados.alfabetoFinal[i]);
+    console.log("}");
+    if (mq.estados[estadoAtual].estadoFinal) {
+        alert("Palavra válida!");
     }
+    else {
+        alert("Palavra inválida!");
+    }
+    console.log("Execução encerrada!");
+    console.log(mq);
 
-    $("#simboloVazio").val(maquinaDeEstados.simboloVazio);
-
-    $("#qtdEstados").val(parseInt(maquinaDeEstados.qtdEstados));
-
-});
+}
